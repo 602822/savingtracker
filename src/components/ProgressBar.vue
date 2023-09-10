3579e9b8b5c1989c22fe1b7eb0438fa214988eaa
602822 <89077126+602822@users.noreply.github.com>
2023-09-10 19:15:59 +0200

    progressbar added

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="progress-container">
+    <div class="progress-bar" :style="{ width: `${progress}%` }"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  progress: {
+    type: Number,
+    required: true,
+    validator: (value: number): boolean => {
+      return value >= 0 && value <= 100;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.progress-container {
+  width: 400px;
+  background-color: #f3f3f3;
+  border-radius: 4px;
+  display: flex;
+  margin-left: 420px;
+  margin-top: 30px;
+}
+
+.progress-bar {
+  height: 20px;
+  background-color: #4caf50;
+  border-radius: 4px;
+}
+</style>
--- src/components/SavingGoal.vue
+++ src/components/SavingGoal.vue
@@ -2,18 +2,37 @@
   <div>
     <img v-if="imageUrl" :src="imageUrl" alt="User selected Image" />
     <img v-else src="../assets/noImage.jpg" alt="No image selected" />
+    <div class="progress">
+      <ProgressBar :progress="progress" />
+      <div class="precentage">
+        <p>{{ progress }}%</p>
+      </div>
+    </div>
 
     <p>{{ savingGoal }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
+import ProgressBar from "./ProgressBar.vue";
 
 const store = useStore();
 const savingGoal = computed(() => store.state.savingGoal);
 const imageUrl = computed(() => store.state.selectedImageUrl);
+
+const progress = ref(50);
 </script>
 
-<style scoped></style>
+<style scoped>
+.progress {
+  display: flex;
+  align-items: center;
+}
+
+.precentage {
+  margin-left: 20px;
+  margin-top: 30px;
+}
+</style>
